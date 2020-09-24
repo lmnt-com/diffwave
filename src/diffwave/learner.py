Bugfix: incorrect noise level calculation during training.
The `noise_level` variable was computed as the square root of alpha
bar but was used later in the code as if it was just plain alpha bar.
Later computations would take the square root of `noise_level` again,
which is incorrect.

This change sets `noise_level` to alpha bar.

Issue: #1

src/diffwave/learner.py
@@ -47,7 +47,7 @@ class DiffWaveLearner:
     self.step = 0
 
     beta = np.array(self.params.noise_schedule)
-    noise_level = np.cumprod(1 - beta)**0.5
+    noise_level = np.cumprod(1 - beta)
     self.noise_level = torch.tensor(noise_level.astype(np.float32))
     self.loss_fn = nn.L1Loss()
     self.summary_writer = None